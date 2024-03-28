996295837d1e8be6097b64078f661641733c3654
PaulHaze <dev@artemist.com.au>
2024-03-28 20:12:37 +1100

    configured config for just used icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,20 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), icon()]
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+		icon({
+			include: {
+				// Include only three `openmoji` icons in the bundle
+				openmoji: ['facebook', 'github', 'twitter'],
+			},
+		}),
+	],
 });
