1f83b669ecb68d46129593a8e4aa47a0591da990
PaulHaze <dev@artemist.com.au>
2024-04-04 07:51:08 +1100

    add logo

--- src/components/react/Logo.tsx
+++ src/components/react/Logo.tsx
@@ -4,8 +4,8 @@ export function NavLogo(props: SVGProps<SVGSVGElement>) {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			width="1.01em"
-			height="1em"
+			width="2em"
+			height="2em"
 			viewBox="0 0 256 255"
 			{...props}
 		>
--- src/components/react/NavBar.tsx
+++ src/components/react/NavBar.tsx
@@ -1,12 +1,18 @@
+import { NavLogo } from './Logo';
+
 export function NavBar() {
 	return (
-		<nav className="flex h-[60px] items-center bg-stone-700/30">
-			<div className="mx-auto flex w-full max-w-lg items-center justify-between px-10 md:px-20">
-				<div className="">Icon</div>
-				<div className="sm:hidden">BURGER</div>
-				<p>Links</p>
-			</div>
+		<div className="">
+			<nav className="flex h-[60px] items-center bg-stone-700/30">
+				<div className="mx-auto flex w-full max-w-lg items-center justify-between px-10 md:px-20">
+					<div className="h-10 w-10 flex-center">
+						<NavLogo />
+					</div>
+					<div className="hidden sm:visible">LINKS</div>
+					<div className="sm:hidden">BURGER</div>
+				</div>
+			</nav>
 			<div className="">HAMBURGER LINKS</div>
-		</nav>
+		</div>
 	);
 }
