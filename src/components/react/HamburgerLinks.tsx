97efcc66944d7f360098db9510cd445e1d8e794b
PaulHaze <dev@artemist.com.au>
2024-04-04 10:38:51 +1100

    move hamburgerstyles into module

--- src/components/react/HamburgerLinks.tsx
+++ src/components/react/HamburgerLinks.tsx
@@ -1,15 +1,19 @@
+import styles from './navStyles.module.scss';
+
 export function HamburgerLinks() {
 	return (
-		<ul className="flex w-full flex-col items-center justify-center bg-stone-700/30 pb-4 text-sm text-stone-400 ">
-			<li className="w-[50%] border-b border-stone-400/50 py-4 text-center hover:text-stone-200">
-				<a href="/">HOME</a>
-			</li>
-			<li className="w-[50%] border-b border-stone-400/50 py-4 text-center">
-				<a href="/blog/">BLOG</a>
-			</li>
-			<li className="w-[50%] pt-4 text-center">
-				<a href="/about/">ABOUT</a>
-			</li>
-		</ul>
+		<div className={styles.hamburgerLinks}>
+			<ul>
+				<li>
+					<a href="/">HOME</a>
+				</li>
+				<li>
+					<a href="/blog/">BLOG</a>
+				</li>
+				<li>
+					<a href="/about/">ABOUT</a>
+				</li>
+			</ul>
+		</div>
 	);
 }
