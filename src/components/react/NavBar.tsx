300654a6345b531c721ccbc2a3386003cb8850b1
PaulHaze <dev@artemist.com.au>
2024-04-04 07:46:03 +1100

    placeholders for react nav components

--- src/components/react/NavBar.tsx
+++ src/components/react/NavBar.tsx
@@ -1,7 +1,12 @@
 export function NavBar() {
 	return (
-		<div>
-			<p>NavBar Placeholder</p>
-		</div>
+		<nav className="flex h-[60px] items-center bg-stone-700/30">
+			<div className="mx-auto flex w-full max-w-lg items-center justify-between px-10 md:px-20">
+				<div className="">Icon</div>
+				<div className="sm:hidden">BURGER</div>
+				<p>Links</p>
+			</div>
+			<div className="">HAMBURGER LINKS</div>
+		</nav>
 	);
 }
---
+++ src/components/react/index.ts
@@ -0,0 +1,4 @@
+export * from './Hamburger';
+export * from './HamburgerLinks';
+export * from './Logo';
+export * from './NavBar';
