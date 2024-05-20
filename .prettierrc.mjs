9bb84add5b30c7a830e3db6440bbcd39ded30eb3
PaulHaze <dev@artemist.com.au>
2024-05-20 11:04:08 +1000

    prettier rule fixes

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,19 +1,19 @@
 /** @type {import("prettier").Config} */
 export default {
-	printWidth: 80,
-	semi: true,
-	singleQuote: true,
-	tabWidth: 2,
-	trailingComma: 'es5',
-	useTabs: true,
-	// pluginSearchDirs: [__dirname],
-	plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-astro'],
-	overrides: [
-		{
-			files: '*.astro',
-			options: {
-				parser: 'astro',
-			},
-		},
-	],
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: true,
+  // pluginSearchDirs: [__dirname],
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
